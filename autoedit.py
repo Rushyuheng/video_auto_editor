08597223b0251895bdb3a5b6f3e135400e85cd30
james_rush <jamesyhh@gmail.com>
2021-05-30 17:05:59 +0800

    add ui

--- autoedit.py
+++ autoedit.py
@@ -3,55 +3,39 @@ from moviepy.editor import VideoFileClip,CompositeVideoClip,ImageClip,concatenat
 from moviepy.video.fx.all import mask_color,fadeout
 from moviepy.audio.fx.all import audio_fadein,audio_fadeout
 
-#manually decide start time and end time
-starmin,startsec = input("start time in min,sec:").split(",") #01,01
-endmin,endsec = input("end time in min,sec:").split(",") #04,11
-icon_path = input("icon_path:")
-starmin = int(starmin)
-startsec = int(startsec)
-endmin = int(endmin)
-endsec = int(endsec)
+def writevideo(startmin,startsec,endmin,endsec,animate_filename,show_filename,icon_filename):
+    # load video source
+    fire = VideoFileClip(animate_filename)
+    masked_fire = mask_color(fire,color=[0,0,0],thr = 50) #filter black background
+    show = VideoFileClip(show_filename).subclip((int(startmin),int(startsec)),(int(endmin),int(endsec)))
 
-'''
-starmin = 1
-startsec = 1
-endmin = 2
-endsec = 1
-icon_path = ""
-'''
+    # add freeze title
+    title_bg = show.to_ImageClip(t=0, with_mask=True, duration= fire.duration) #freeze show background
+    masked_fire = masked_fire.resize(title_bg.size)
+    title = CompositeVideoClip([title_bg,masked_fire.set_position("center")], use_bgclip=False)
 
-# load video source
-fire = VideoFileClip("./fire.m4v")
-masked_fire = mask_color(fire,color=[0,0,0],thr = 50) #filter black background
-show = VideoFileClip("./back.mp4").subclip((starmin,startsec),(endmin,endsec))
+    #fade in audio
+    show = audio_fadein(show,5)
 
-# add freeze title
-title_bg = show.to_ImageClip(t=0, with_mask=True, duration= fire.duration) #freeze show background
-masked_fire = masked_fire.resize(title_bg.size)
-title = CompositeVideoClip([title_bg,masked_fire.set_position("center")], use_bgclip=False)
+    # use icon img or middle frame of animate
+    if icon_filename:
+        icon = ImageClip(icon_filename).set_duration(show.duration - 3)
+    else:
+        icon = masked_fire.to_ImageClip(t= fire.duration / 2, with_mask=True, duration= show.duration - 3)
+    #add icon to corner
 
-#fade in audio
-show = audio_fadein(show,5)
+    icon = icon.crossfadein(3)
+    icon = icon.crossfadeout(3)
+    icon = icon.resize(width=256)
+    show_with_icon = CompositeVideoClip([show,icon.set_position(("left","bottom"))], use_bgclip=False)
 
-# use icon img or middle frame of animate
-if icon_path:
-    icon = ImageClip(icon_path).set_duration(show.duration - 3)
-else:
-    icon = masked_fire.to_ImageClip(t= fire.duration / 2, with_mask=True, duration= show.duration - 3)
-#add icon to corner
+    #final concat
+    final_clip = concatenate_videoclips([title,show_with_icon])
 
-icon = icon.crossfadein(3)
-icon = icon.crossfadeout(3)
-icon = icon.resize(width=256)
-show_with_icon = CompositeVideoClip([show,icon.set_position(("left","bottom"))], use_bgclip=False)
+    #final fadeout
+    final_clip = audio_fadeout(final_clip,5)
+    final_clip = fadeout(final_clip,3)
 
-#final concat
-final_clip = concatenate_videoclips([title,show_with_icon])
-
-#final fadeout
-final_clip = audio_fadeout(final_clip,5)
-final_clip = fadeout(final_clip,3)
-
-#write final clip
-final_clip.write_videofile("test.mp4", fps=30)
-final_clip.close()
+    #write final clip
+    final_clip.write_videofile("test.mp4", fps=30)
+    final_clip.close()
---
+++ main.py
@@ -0,0 +1,77 @@
+import sys
+import PyQt5.uic as uic
+from PyQt5.QtWidgets import QFileDialog, QMainWindow, QApplication
+from PyQt5.QtCore import pyqtSlot
+import autoedit
+
+qtCreatorFile = "main.ui" # Enter file here.
+ 
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
+class MainUi(QMainWindow, Ui_MainWindow):
+	def __init__(self):
+		QMainWindow.__init__(self)
+		Ui_MainWindow.__init__(self)
+		self.setupUi(self)
+		self.iniGuiEvent()
+
+	def iniGuiEvent(self):# connect all button to all event slot
+		self.write_video.clicked.connect(self.write_video_onClick)
+		self.choose_show.clicked.connect(self.choose_show_onClick)
+		self.choose_animate.clicked.connect(self.choose_animate_onClick)
+		self.choose_icon.clicked.connect(self.choose_icon_onClick)
+		self.animate_filename = ""
+		self.show_filename = ""
+		self.icon_filename = ""
+
+	@pyqtSlot()
+	def choose_show_onClick(self):
+		self.show_filename,_ = QFileDialog.getOpenFileName(self,"open file","./","vidoe file:(*.wmv *.mp4 *.m4v)")
+		if not self.show_filename:
+			self.show_path.setText("尚未選擇檔案")
+		else:
+			self.show_path.setText(self.show_filename)
+
+	@pyqtSlot()
+	def choose_animate_onClick(self):
+		self.animate_filename,_ = QFileDialog.getOpenFileName(self,"open file","./","vidoe file:(*.wmv *.mp4 *.m4v)")
+		if not self.animate_filename:
+			self.animate_path.setText("尚未選擇檔案")
+		else:
+			self.animate_path.setText(self.animate_filename)
+
+	@pyqtSlot()
+	def choose_icon_onClick(self):
+		self.icon_filename,_ = QFileDialog.getOpenFileName(self,"open file","./","image file:(*.png *.jpg *.jpeg)")
+		if not self.icon_filename:
+			self.icon_path.setText("尚未選擇檔案")
+		else:
+			self.icon_path.setText(self.icon_filename)
+
+	@pyqtSlot()
+	def write_video_onClick(self):
+		#get value from line edit object
+		start_min = self.start_min.text()
+		start_sec = self.start_sec.text()	
+		end_min = self.end_min.text()
+		end_sec = self.end_sec.text()
+
+		if not (start_min and start_sec and end_min and end_sec):
+			self.status.setText("錯誤：未設定時間")
+		elif not self.show_filename:
+			self.status.setText("錯誤：未選擇表演影片")
+		elif not self.animate_filename:
+			self.status.setText("錯誤：未選擇片頭動畫")
+		else:
+			self.status.setText("輸出影片中...")
+			self.status.repaint()
+			autoedit.writevideo(start_min,start_sec,end_min,end_sec,self.animate_filename,self.show_filename,self.icon_filename)
+			self.status.setText("影片輸出成功")
+
+if __name__ == "__main__": #main function
+	def run_app():
+		app = QApplication(sys.argv)
+		window = MainUi()
+		window.show()
+		app.exec_()
+	run_app()
