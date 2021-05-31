b028e7da8931f09b323408f71e07fecb53ebdabf
james_rush <jamesyhh@gmail.com>
2021-05-31 21:25:39 +0800

    V2

--- autoedit.py
+++ autoedit.py
@@ -3,15 +3,18 @@ from moviepy.editor import VideoFileClip,CompositeVideoClip,ImageClip,concatenat
 from moviepy.video.fx.all import mask_color,fadeout
 from moviepy.audio.fx.all import audio_fadein,audio_fadeout
 
-def writevideo(startmin,startsec,endmin,endsec,animate_filename,show_filename,icon_filename):
+def writevideo(index,show_filename,start_time,end_time,animate_filename,enable_blackfilter,icon_filename,icon_pos,icon_size):
     # load video source
     fire = VideoFileClip(animate_filename)
-    masked_fire = mask_color(fire,color=[0,0,0],thr = 50) #filter black background
-    show = VideoFileClip(show_filename).subclip((int(startmin),int(startsec)),(int(endmin),int(endsec)))
+    if enable_blackfilter:
+        masked_fire = mask_color(fire,color=[0,0,0],thr = 50) #filter black background
+    else:
+        masked_fire = fire
+    show = VideoFileClip(show_filename).subclip((start_time),(end_time))
 
     # add freeze title
     title_bg = show.to_ImageClip(t=0, with_mask=True, duration= fire.duration) #freeze show background
-    masked_fire = masked_fire.resize(title_bg.size)
+    masked_fire = masked_fire.resize(title_bg.size).crossfadeout(3)
     title = CompositeVideoClip([title_bg,masked_fire.set_position("center")], use_bgclip=False)
 
     #fade in audio
@@ -26,8 +29,8 @@ def writevideo(startmin,startsec,endmin,endsec,animate_filename,show_filename,ic
 
     icon = icon.crossfadein(3)
     icon = icon.crossfadeout(3)
-    icon = icon.resize(width=256)
-    show_with_icon = CompositeVideoClip([show,icon.set_position(("left","bottom"))], use_bgclip=False)
+    icon = icon.resize(icon_size)
+    show_with_icon = CompositeVideoClip([show,icon.set_position(icon_pos)], use_bgclip=False)
 
     #final concat
     final_clip = concatenate_videoclips([title,show_with_icon])
@@ -37,5 +40,5 @@ def writevideo(startmin,startsec,endmin,endsec,animate_filename,show_filename,ic
     final_clip = fadeout(final_clip,3)
 
     #write final clip
-    final_clip.write_videofile("test.mp4", fps=30)
+    final_clip.write_videofile("result_" + str(index) + ".mp4", fps=30)
     final_clip.close()
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import PyQt5.uic as uic
-from PyQt5.QtWidgets import QFileDialog, QMainWindow, QApplication
+from PyQt5.QtWidgets import QFileDialog, QMainWindow, QApplication, QTableWidgetItem
 from PyQt5.QtCore import pyqtSlot
 import autoedit
 
@@ -16,13 +16,21 @@ class MainUi(QMainWindow, Ui_MainWindow):
 		self.iniGuiEvent()
 
 	def iniGuiEvent(self):# connect all button to all event slot
-		self.write_video.clicked.connect(self.write_video_onClick)
 		self.choose_show.clicked.connect(self.choose_show_onClick)
 		self.choose_animate.clicked.connect(self.choose_animate_onClick)
 		self.choose_icon.clicked.connect(self.choose_icon_onClick)
+		self.blackfilter_checkbox.clicked.connect(self.blackfilter_checkboxChangedAction)
+		self.icon_pos_combobox.activated.connect(self.icon_pos_comboboxActivate)
+		self.add_queue.clicked.connect(self.add_queue_onClick)
+		self.clear_table.clicked.connect(self.clear_table_onClick)
+		self.write_video.clicked.connect(self.write_video_onClick)
+
+		#intialize parameter
 		self.animate_filename = ""
 		self.show_filename = ""
 		self.icon_filename = ""
+		self.enable_blackfilter = True
+		self.icon_pos = "左下"
 
 	@pyqtSlot()
 	def choose_show_onClick(self):
@@ -50,24 +58,99 @@ class MainUi(QMainWindow, Ui_MainWindow):
 			self.icon_path.setText(self.icon_filename)
 
 	@pyqtSlot()
-	def write_video_onClick(self):
+	def icon_pos_comboboxActivate(self):
+		self.icon_pos = self.icon_pos_combobox.currentText()
+
+	@pyqtSlot()
+	def blackfilter_checkboxChangedAction(self):
+		if self.blackfilter_checkbox.isChecked():
+			self.enable_blackfilter = True
+		else:
+			self.enable_blackfilter = False
+
+	@pyqtSlot()
+	def add_queue_onClick(self):
 		#get value from line edit object
 		start_min = self.start_min.text()
 		start_sec = self.start_sec.text()	
 		end_min = self.end_min.text()
 		end_sec = self.end_sec.text()
+		start_time = start_min + ":" + start_sec
+		end_time = end_min + ":"  + end_sec
+
+		icon_width = self.icon_width.text()
+		icon_height = self.icon_height.text()
+		icon_size = icon_width + "X" + icon_height
+
 
 		if not (start_min and start_sec and end_min and end_sec):
 			self.status.setText("錯誤：未設定時間")
+		elif not (icon_width and icon_height):
+			self.status.setText("錯誤：未設定ICON尺寸")
 		elif not self.show_filename:
 			self.status.setText("錯誤：未選擇表演影片")
 		elif not self.animate_filename:
 			self.status.setText("錯誤：未選擇片頭動畫")
 		else:
-			self.status.setText("輸出影片中...")
+			rowPosition = self.processqueue.rowCount()
+			self.processqueue.insertRow(rowPosition)
+			self.processqueue.setItem(rowPosition , 0, QTableWidgetItem(self.show_filename))
+			self.processqueue.setItem(rowPosition , 1, QTableWidgetItem(start_time))
+			self.processqueue.setItem(rowPosition , 2, QTableWidgetItem(end_time))
+			self.processqueue.setItem(rowPosition , 3, QTableWidgetItem(self.animate_filename))
+			self.processqueue.setItem(rowPosition , 4, QTableWidgetItem(str(self.enable_blackfilter)))
+			self.processqueue.setItem(rowPosition , 5, QTableWidgetItem(self.icon_filename))
+			self.processqueue.setItem(rowPosition , 6, QTableWidgetItem(self.icon_pos))
+			self.processqueue.setItem(rowPosition , 7, QTableWidgetItem(icon_size))
+			self.status.setText("成功加入影片處理清單")
+
+	@pyqtSlot()
+	def clear_table_onClick(self):
+		rowlength = self.processqueue.rowCount()
+		for index in range(0,rowlength):
+			self.processqueue.removeRow(0)
+		self.status.setText("已清除影片處理清單")
+
+	@pyqtSlot()
+	def write_video_onClick(self):
+		rowlength = self.processqueue.rowCount()
+		for index in range(0,rowlength):
+			#parse table content
+			show_filename = str(self.processqueue.item(index,0).text())
+
+			start_time = str(self.processqueue.item(index,1).text()).split(":")
+			start_time = (int(start_time[0]),int(start_time[1]))
+
+			end_time = str(self.processqueue.item(index,2).text()).split(":")
+			end_time = (int(end_time[0]),int(end_time[1]))
+
+			animate_filename = str(self.processqueue.item(index,3).text())
+
+			enable_blackfilter = str(self.processqueue.item(index,4).text())
+			if enable_blackfilter == "False":
+				enable_blackfilter = False # cast to bool
+			else:
+				enable_blackfilter = True
+			
+			icon_filename = str(self.processqueue.item(index,5).text())
+			
+			icon_pos = str(self.processqueue.item(index,6).text())
+			if icon_pos == "右下":
+				icon_pos = ("right","bottom")
+			elif icon_pos == "右上":
+				icon_pos = ("right","top")
+			elif icon_pos == "左上":
+				icon_pos = ("left","top")
+			else:
+				icon_pos = ("left","bottom")
+
+			icon_size = str(self.processqueue.item(index,7).text()).split("X")
+			icon_size = (int(icon_size[0]),int(icon_size[1])) # API resize (width,height)
+
+			self.status.setText("輸出影片中...({0}/{1})".format(index + 1,rowlength))
 			self.status.repaint()
-			autoedit.writevideo(start_min,start_sec,end_min,end_sec,self.animate_filename,self.show_filename,self.icon_filename)
-			self.status.setText("影片輸出成功")
+			autoedit.writevideo(index,show_filename,start_time,end_time,animate_filename,enable_blackfilter,icon_filename,icon_pos,icon_size)
+		self.status.setText("已輸出所有影片")
 
 if __name__ == "__main__": #main function
 	def run_app():
